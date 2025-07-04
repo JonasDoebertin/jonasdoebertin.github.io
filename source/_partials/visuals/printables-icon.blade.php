6dee6e55577a2e43f582be4b873e2622f39ba443
Jonas Döbertin <jonas@doebertin.de>
2025-07-04 20:24:38 +0200

    add printables link

---
+++ source/_partials/visuals/printables-icon.blade.php
@@ -0,0 +1,10 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    @if($class) class="{{ $class }}" @endif
+    aria-label="Printables Logo"
+    style="fill: currentColor;"
+    role="img"
+>
+    <path d="M3.678 4.8 12 9.6v9.6l8.322-4.8V4.8L12 0ZM12 19.2l-8.322-4.8V24Z"/>
+</svg>
